6ef07fffe7dbb93500edbdef96914099d307fc38
Jimmy <121305329+quuixly@users.noreply.github.com>
2023-02-09 10:40:05 +0100

    Add basic animations and remove unnecessary files.

---
+++ main.js
@@ -0,0 +1,7 @@
+function btnOnMouseOver() {
+    document.getElementsByClassName("nav_btn")[0].style.color = "black";
+}
+
+function btnOnMouseOut () {
+    document.getElementsByClassName("nav_btn")[0].style.color = "#03C988";
+}
